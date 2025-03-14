289d69bb5fce37d2ccc1a61a36f675d80d7df3b6
IlanTauscher <48240500@est.ort.edu.ar>
2025-03-14 12:14:12 -0300

    Add files via upload

---
+++ Nivelacion.js
@@ -0,0 +1,55 @@
+//Ejercicio 1: 
+// let nombre = prompt("Ingrese su nombre");
+// Console.log("Hola", nombre);
+
+// Ejercicio 2:
+// let num1 = parseInt(prompt("Ingrese un numero"));
+// let num2 = parseInt(prompt("Ingrese otro numero"));
+// console.log("La suma es:" , num1 + num2);
+// console.log("La resta es:" , num1 - num2);
+// console.log("La multiplicacion es:" , num1 * num2);
+// console.log("La divicion es:" , num1 / num2);
+
+// Ejercicio 3:
+// let num1 = parseInt(prompt("Ingrese un numero"));
+// if(num1 % 2 == 0)
+// {
+//     console.log("Su numero es par");
+// }
+// else{
+//     console.log("Su numero es impar");
+// }
+
+// Ejercicio 4: 
+// let palabra = document.querySelector(".palabra")
+// let btn = document.querySelector(".btn")
+
+// btn.addEventListener("click", () => {
+// console.log(palabra.value.length)
+// });
+
+// Ejercico 5:
+// let btn = document.querySelector(".btn")
+
+// btn.addEventListener("click", () => {
+//     let frase = document.querySelector(".frase").value
+//     let num = document.querySelector(".num").value
+
+//     for(i = 0; i < num; i++){
+//         console.log(frase)
+//     }
+// });
+
+// Ejercicio 6:
+// let btn = document.querySelector(".btn")
+
+// btn.addEventListener("click", () => {
+// let num = document.querySelector(".num").value
+// for(i = 0; i <= 10; i++){
+//     console.log(num * i)
+// }
+// })
+
+// Ejercicio 7:
+
+Math.floor((Math.random() * 10 )+ 1);
